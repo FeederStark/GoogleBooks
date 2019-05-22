b32ccca23be9731979cf4ccee831935ccc887cae
Lucas <lucasrafael431@gmail.com>
2019-05-22 15:34:48 -0300

    update main button style

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,13 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Button } from './styles';
 
 function Main() {
-  const [overDiv, setOverDiv] = useState(false);
   return (
-    <Container overDiv={overDiv}>
-      <div onMouseEnter={() => setOverDiv(true)} onMouseLeave={() => setOverDiv(false)}>
-        <Button to="/home">ACESSAR</Button>
-      </div>
+    <Container>
+      <Button to="/home">ACESSAR</Button>
     </Container>
   );
 }
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -6,23 +6,57 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-
-  background: ${props => props.overDiv && '#ffecb3'};
-
-  div {
-    width: 100%;
-    height: 200px;
-    display: flex;
-    justify-content: center;
-    cursor: pointer;
-  }
 `;
 
 export const Button = styled(Link)`
-  background: none;
-  border: 0;
-  font-size: 80px;
-  color: #212121;
-  cursor: pointer;
+  text-align: center;
+  text-transform: uppercase;
   text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  font-size: 80px;
+  letter-spacing: 4px;
+  position: relative;
+  background-color: #ffe207;
+  border: none;
+  border-radius: 10px;
+  color: #fff;
+  padding: 10px;
+  width: 100%;
+  height: 400px;
+  text-align: center;
+  transition-duration: 0.4s;
+  overflow: hidden;
+  /* box-shadow: 0 5px 15px #193047; */
+  border-radius: 4px;
+  &:hover {
+    background: #fff;
+    box-shadow: 0px 2px 10px 5px #1abc9c;
+    color: #000;
+  }
+  &:after {
+    content: '';
+    background: #1abc9c;
+    display: block;
+    position: absolute;
+    padding-top: 300%;
+    padding-left: 350%;
+    margin-left: -20px !important;
+    margin-top: -120%;
+    opacity: 0;
+    transition: all 0.8s;
+  }
+  &:active {
+    &::after {
+      padding: 0;
+      margin: 0;
+      opacity: 1;
+      transition: 0s;
+    }
+  }
+  &:focus {
+    outline: 0;
+  }
 `;
